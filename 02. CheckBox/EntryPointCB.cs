24588ea7c316893ffe6ec7ee907367b42cc0aa76
Deboh <david.ebohttu@gmail.com>
2018-03-09 10:50:01 -0600

    Add project files.

---
+++ 02. CheckBox/EntryPointCB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class EntryPointCB
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement checkBox;
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/check-button-test-3/";
+        string option = "1";
+
+        driver.Navigate().GoToUrl(url);
+        checkBox = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=\"checkbox\"]:nth-child(" + option + ")"));
+        // This selector will change to match the proper element used in Line 13
+
+        /*
+        if (checkBox.GetAttribute("checked") == "true") //Checks to see if the checkBox is checked
+        // Will either return true or nothing at all
+        {
+            Console.WriteLine("The Check Box is checked"); //Checked
+        }   
+        else
+        {
+            Console.WriteLine("The Check Box is not checked"); //Not Checked
+        }
+        */
+
+        Console.WriteLine(checkBox.GetAttribute("value"));
+
+        option = "3";
+        checkBox = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=\"checkbox\"]:nth-child(" + option + ")"));
+        Console.WriteLine(checkBox.GetAttribute("value"));
+
+        checkBox.Click();
+
+        Thread.Sleep(5000);
+
+        driver.Quit();
+
+
+
+    }
+
+}
+
---
+++ SpecialElements/EntryPointSE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPointSE
+{
+    static IWebDriver driver = new ChromeDriver(); //driver needs to be made static
+    static IWebElement textBox; 
+
+    static void Main()
+    {
+        string url = "http://testing.todorvachev.com/special-elements/text-input-field/";
+
+        driver.Navigate().GoToUrl(url);
+        textBox = driver.FindElement(By.Name("username"));
+        textBox.SendKeys("Test text"); //Sends specific string of text to Text Box
+
+        Thread.Sleep(3000); //3 second pause
+        //textBox.Clear(); // Clears Text Box
+        Console.WriteLine(textBox.GetAttribute("value")); //Whatever is written in textbox will go under the attribute value
+        Console.WriteLine(textBox.GetAttribute("maxlength")); //The maximum number of text allowed in the text box will be printed on the console
+        Thread.Sleep(3000); //3 second pause
+
+        driver.Quit();
+    }
+}
